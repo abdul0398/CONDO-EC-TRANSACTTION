947d3cb060bd3e9f7d67afefeabecda4caf6de47
abdul0398 <abdulrkhan570@gmail.com>
2024-05-23 22:33:14 +0530

    feat: added sorting in table

--- src/components/Dashboard/transaction-table/Main.tsx
+++ src/components/Dashboard/transaction-table/Main.tsx
@@ -1,8 +1,8 @@
-import React, { use, useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { MyContext } from "@/context/context";
-import { useContext } from "react";
 import dynamic from "next/dynamic";
 import { ListChildComponentProps } from "react-window";
+import { FaSort } from "react-icons/fa";
 
 const List = dynamic(
   () => import("react-window").then((mod) => mod.FixedSizeList),
@@ -13,10 +13,18 @@ const List = dynamic(
 
 export default function Transactions() {
   const { transactions } = useContext(MyContext);
+  const [listings, setListings] = useState<any[]>(transactions.slice(0, 25000));
+  const [sortConfig, setSortConfig] = useState<{
+    key: string;
+    direction: string;
+  } | null>(null);
 
-  // Define the Row component with proper types for props
+  useEffect(() => {
+    setListings(transactions.slice(0, 25000));
+  }, [transactions]);
+
   const Row: React.FC<ListChildComponentProps> = ({ index, style }) => {
-    const transaction = transactions[index];
+    const transaction = listings[index];
 
     if (!transaction) {
       return null; // Return null if transaction is not available
@@ -81,30 +89,140 @@ export default function Transactions() {
     );
   };
 
+  const handleSort = (key: string) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+
+    const sortedListings = [...listings].sort((a, b) => {
+      const compare = (val1: any, val2: any) => {
+        if (val1 < val2) {
+          return direction === "ascending" ? -1 : 1;
+        }
+        if (val1 > val2) {
+          return direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      };
+
+      if (key === "project") {
+        return compare(a.project, b.project);
+      } else if (key === "price") {
+        return compare(parseInt(a.price), parseInt(b.price));
+      } else if (key === "psf") {
+        const psfA = parseInt(a.price) / (parseInt(a.area) * 10.7);
+        const psfB = parseInt(b.price) / (parseInt(b.area) * 10.7);
+        return compare(psfA, psfB);
+      } else if (key === "area") {
+        return compare(parseInt(a.area), parseInt(b.area));
+      } else if (key === "month") {
+        return compare(a.contractDate, b.contractDate);
+      } else if (key === "street") {
+        return compare(a.street, b.street);
+      } else if (key === "district") {
+        return compare(a.district, b.district);
+      }
+      return 0;
+    });
+
+    setSortConfig({ key, direction });
+    setListings(sortedListings);
+  };
   return (
     <div className="min-w-full text-left text-sm font-light overflow-hidden">
       <div className="border-b font-medium dark:border-neutral-500 grid gap-1 grid-cols-[7%_10%_10%_7%_7%_5%_7%_9%_7%_10%_7%_7%] text-sm">
         <div className="px-1 py-4 text-xs">Type of Sale</div>
-        <div className="px-1 py-4 text-xs">Project</div>
+        <div className="px-1 py-4  text-xs">
+          <div className="flex">
+            Project
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("project")}
+              />
+            </span>
+          </div>
+        </div>
         <div className="px-1 py-4 text-xs">Tenure</div>
-        <div className="px-1 py-4 text-xs">Price($)</div>
-        <div className="px-1 py-4 text-xs">PSF($)</div>
+        <div className="px-1 py-4 text-xs">
+          <div className="flex">
+            Price($)
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("project")}
+              />
+            </span>
+          </div>
+        </div>
+        <div className="px-1 py-4 text-xs">
+          <div className="flex">
+            PSF($)
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("psf")}
+              />
+            </span>
+          </div>
+        </div>
         <div
           className="px-1 py-4 text-xs hover:cursor-pointer"
           title="Area(Sqft)"
         >
-          Are...
+          <div className="flex">
+            Area
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("area")}
+              />
+            </span>
+          </div>
         </div>
         <div
           className="px-1 py-4 text-xs hover:cursor-pointer"
           title="MonthYear"
         >
-          Mont...
+          <div className="flex">
+            Month
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("month")}
+              />
+            </span>
+          </div>
         </div>
         <div className="px-1 py-4 text-xs">Project Type</div>
         <div className="px-1 py-4 text-xs">Market Segment</div>
-        <div className="px-1 py-4 text-xs">Street</div>
-        <div className="px-1 py-4 text-xs">District</div>
+        <div className="px-1 py-4 text-xs">
+          <div className="flex">
+            Street
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("street")}
+              />
+            </span>
+          </div>
+        </div>
+        <div className="px-1 py-4 text-xs">
+          <div className="flex">
+            District
+            <span className="flex items-center ms-2">
+              <FaSort
+                className="hover:cursor-pointer"
+                onClick={() => handleSort("district")}
+              />
+            </span>
+          </div>
+        </div>
         <div className="px-1 py-4 text-xs">Type of Area</div>
       </div>
       <div className="overflow-hidden">
